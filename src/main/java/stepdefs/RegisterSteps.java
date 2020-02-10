8b73e70f6dd7ceef2445d06e3ed7f9ee9559bd1d
dangnh007 <54935533+dangnh007@users.noreply.github.com>
2020-02-10 22:40:07 +0700

    Commit A1

--- src/main/java/stepdefs/RegisterSteps.java
+++ src/main/java/stepdefs/RegisterSteps.java
@@ -3,6 +3,7 @@ package stepdefs;
 import controllers.Controller;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import jdk.jfr.Description;
 import workflows.RegisterPage;
 
 public class RegisterSteps {
@@ -13,7 +14,7 @@ public class RegisterSteps {
         this.controller = controller;
         registerPage = new RegisterPage(this.controller.getDriver());
     }
-
+    @Description("Test")
     @When("^I access to Register Function$")
     public void gotoRegisterPage(){
         registerPage.clickOnAccountDropdown();
